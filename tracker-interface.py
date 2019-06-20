3e4ea5ce82c21f5810c79e729bc73e1ad0c907bd
Helmut Lord <kellyhlord@gmail.com>
2019-06-20 11:49:26 -0400

    Implemented functions for general cleanup of program.  Completed Case Status requests.  Working on Evidence Request form system.

--- tracker-interface.py
+++ tracker-interface.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
 import time
 import os
-import calendar
+import datetime
 from selenium import *
 # DEF FUNCTIONS #
 
@@ -59,6 +59,13 @@ def try_click_on_load(waitElement):
         elem.click()
 
 
+def try_find_element(waitElement):
+    if len(driver.find_elements_by_xpath(waitElement)) == 0:
+        return False
+    else:
+        return True
+
+
 def search_case_from_case_num(caseNum):
     driver.get("https://tracker4.pacga.org/search?utf8=%E2%9C%93&search%5Blast_name%5D=&search%5Bfi"
                "rst_name%5D=&search%5Bmiddle_name%5D=&search%5Bcase_role_kwid%5D=&search%5Bda_num%5D=&s"
@@ -67,24 +74,132 @@ def search_case_from_case_num(caseNum):
                "5Bfbi_num%5D=&search%5Bgdc_num%5D=&search%5Bdoc_ef_num%5D=&search%5Bjail_num%5D=&search%5Bstatus_note%"
                "5D=&search%5Boca_num%5D=&search%5Bcrimelab_num%5D=&search%5Bfile_num%5D=&search%5Bcounty_id")
 
-# DEF FUNCTIONS #
+
+def driver_setup():
+    driver.set_window_position(-2000, 0)
+    # Install requisite Tracker plugin
+    ext_dir = os.path.abspath("trackerhelper5@pacga.org.xpi")
+    driver.install_addon(ext_dir, temporary=True)
+    # Get tracker homepage
+    driver.get("http://tracker4.pacga.org")
+
+
+def go_to_case(neededValues):
+    baseYear = neededValues[0]
+    year = baseYear[-2] + baseYear[-1]
+    caseNum = neededValues[1]
+    fullCaseNum = "ST-" + str(year) + "-CR-" + str(caseNum)
+    search_case_from_case_num(fullCaseNum)
+    try_click_on_load("/html/body/div[1]/div[2]/div[1]/table/tbody/tr/th/div[2]/table/tbody/tr[3]/td[1]/span/a")
+
+
+def set_victim_services(textToEnter):
+    casePage = driver.current_url
+    while True:
+        events = window.Read()
+        if events[0] == "VSERV":
+            driver.switch_to.window(driver.window_handles[0])
+            driver.get(casePage + "/victims")
+            if driver.find_element_by_xpath("//*[contains(text(), 'No victims.')]"):
+                break
+            x = 0
+            while True:
+                try_click_on_load("/html/body/div[1]/div[2]/div[1]/table/tbody/tr/th/table[2]"
+                                  "/tbody/tr[1]/td/table/tbody/tr/td[2]/a[1]")
+                elem = driver.find_elements_by_xpath("//*[contains(text(), 'Victim:')]")
+                print([elements.text for elements in elem])
+                try_click_on_load("/html/body/div[1]/div[2]/div[1]/table/tbody/tr/th/table[2]"
+                                  "/tbody/tr[1]/td/table/tbody/tr/td[2]/a[1]")
+                elem = driver.find_elements_by_xpath("//*[contains(text(), 'Victim:')]")
+                elem[x].click()
+                try_click_on_load("//*[@id=\"vssr_service_kwid_1212\"]")
+                try_click_on_load("//*[@id=\"vssr_service_kwid_1211\"]")
+                try_text_on_load("//*[@id=\"victim_service_service_note\"]", textToEnter)
+                if x == len(elem) - 1:
+                    break
+                x += 1
+        elif events[0] == "Submit":
+            break
+        elif events[0] == "Cancel":
+            break
+        else:
+            continue
+
+
+def letter_setup(textToEnter, pageURL):
+    window.Element('VSERV').Update(visible=True)
+
+    # Initialize variables
+    go_to_case(values)
+
+    # Now, get the documents page for this case.
+
+    casePage = driver.current_url
+    documentsPage = casePage + pageURL
+
+    # Open new tab
+    driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 't')
+    # Set new tab as focused tab
+    driver.switch_to.window(driver.window_handles[-1])
+
+    # Open documents page
+    driver.execute_script("window.open(\'" + documentsPage + "\')")
+    set_victim_services(textToEnter)
+    driver.switch_to.window(driver.window_handles[0])
+
+
+def erq_setup(pageURL):
+    # Now, get the documents page for this case.
+
+    casePage = driver.current_url
+    documentsPage = casePage + pageURL
+
+    # Open new tab
+    driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 't')
+    # Set new tab as focused tab
+    driver.switch_to.window(driver.window_handles[-1])
+    # Open documents page
+    driver.execute_script("window.open(\'" + documentsPage + "\')")
+    dates = find_later_date(14)
+    print(dates)
+    driver.switch_to.window(driver.window_handles[-1])
+    time.sleep(1)
+    elem = driver.find_element_by_xpath("/html/body/div[1]/div[2]/div[1]/table/tbody/tr/th/form/table/tbody/tr[4]/td[2]/input")
+    elem.send_keys(dates)
+    time.sleep(.01)
+    elem.send_keys(Keys.RETURN)
+    driver.switch_to.window(driver.window_handles[0])
+
+
+def find_later_date(distanceInTime):
+    currentDate = datetime.datetime.now()
+    requestByDate = str(currentDate + datetime.timedelta(days=distanceInTime))
+    formattedRequestByDate = requestByDate[5:7]+"/"+requestByDate[8:10]+"/"+requestByDate[0:4]
+    return formattedRequestByDate
+
+# END DEF FUNCTIONS #
+
 
 # Initialize the browser
 driver = webdriver.Firefox(executable_path='geckodriver.exe')
-driver.set_window_position(-2000, 0)
-# Install requisite Tracker plugin
-ext_dir = os.path.abspath("trackerhelper5@pacga.org.xpi")
-driver.install_addon(ext_dir, temporary=True)
-# Get tracker homepage
-driver.get("http://tracker4.pacga.org")
-# Call Login Function
-
+driver_setup()
 
 # Defines the style for submit and cancel buttons in this program.
 submitButton = [
-    [sg.Button(button_text="Submit", auto_size_button=True, button_color=('#FFFFFF', '#483D8B'), focus=True)]]
+    [sg.Button(button_text="Submit",
+               auto_size_button=True,
+               button_color=('#FFFFFF', '#483D8B'),
+               focus=True)
+     ]
+]
 cancelButton = [
-    [sg.Button(button_text="Cancel", auto_size_button=True, button_color=('#FFFFFF', '#483D8B'))]]
+    [sg.Button(button_text="Cancel",
+               auto_size_button=True,
+               button_color=('#FFFFFF',
+                             '#483D8B')
+               )
+     ]
+]
 
 # Establishes layout for Login form.
 loginLayout = [[sg.Text('Tracker Interface - Login')],
@@ -92,17 +207,21 @@ loginLayout = [[sg.Text('Tracker Interface - Login')],
                [sg.InputText()],
                [sg.Text('Password')],
                [sg.InputText(password_char="*")],  # password_char hides text with *
-               [sg.Column(submitButton), sg.Column(cancelButton)]
+               [sg.Column(submitButton),
+                sg.Column(cancelButton)]
                ]
 
 # Names window as 'Tracker Interface - Login' and assigns its layout to loginLayout.
 window = sg.Window('Tracker Interface - Login', icon="SEAL.jpg").Layout(loginLayout)
+
 # Reads button events and values entered, stores them in variables.
 event, values = window.Read()
+
 # If cancel button is pressed, the program is closed.
 if event == "Cancel":
     print("THIS SHOULD CLOSE")
     window.Close()
+    driver.close()
     exit()
 elif event == "Submit":
     window.Close()
@@ -122,167 +241,51 @@ jobMakerLayout = [
     [sg.Column(columnTop)],
     [sg.Text('_' * 100)],
     [sg.Text('Case Options:')],
-    [sg.Radio('Display Case', "RADIO1", default=True), sg.Radio('CC-JT Letter Creator', "RADIO1"),
+    [sg.Radio('Display Case', "RADIO1", default=True),
+     sg.Radio('CC-JT Letter Creator', "RADIO1"),
      sg.Radio('CC Letter Creator', "RADIO1"),
      sg.Radio('Evidence Request Creator (WIP)', "RADIO1")],
-    [sg.Radio('Case Status Updater', "RADIO1"),
-     sg.Radio('Change of Plea Updater', "RADIO1")],
+    [sg.Radio('Case Status/Change of Plea Updater', "RADIO1")],
     [sg.Column(submitButton),
-     sg.Button(button_text="Add victim services?", button_color=('#FFFFFF', '#7766E8'), visible=False, key='VSERV'),
-     sg.Column(cancelButton)]
+     sg.Button(button_text="Add victim services?",
+               button_color=('#FFFFFF', '#7766E8'),
+               visible=False, key='VSERV'),
+     sg.Column(cancelButton)],
+    [sg.Text('Evidence request form already made.', visible=False, key='ERQPRESENT')]
 ]
 
 window = sg.Window('Tracker Interface - Case Search', icon="SEAL.jpg").Layout(jobMakerLayout)
 while True:
     event, values = window.Read()
+    print(event)
+    if event[0] == "C":
+        window.Close()
+        driver.quit()
+        exit()
     # Makes button invisible again.
     print(values)
+    window.Element('ERQPRESENT').Update(visible=False)
     window.Element('VSERV').Update(visible=False)
     window.VisibilityChanged()
     # Brings web browser back into view.
     driver.set_window_position(0, 0)
     if values[2]:
-        baseYear = values[0]
-        year = baseYear[-2] + baseYear[-1]
-        caseNum = values[1]
-        fullCaseNum = "ST-" + str(year) + "-CR-" + str(caseNum)
-        search_case_from_case_num(fullCaseNum)
-        try_click_on_load("/html/body/div[1]/div[2]/div[1]/table/tbody/tr/th/div[2]/table/tbody/tr[3]/td[1]/span/a")
+        go_to_case(values)
     elif values[3]:
-        window.Element('VSERV').Update(visible=True)
-        # Initialize variables
-        baseYear = values[0]
-        year = baseYear[-2]+baseYear[-1]
-        caseNum = values[1]
-        numJTDates = ""
-        jtDateSecond = ""
-        # Construct case number
-        fullCaseNum = "ST-" + str(year) + "-CR-" + str(caseNum)
-
-        # Try to enter case number
-
-        search_case_from_case_num(fullCaseNum)
-
-        # Click on case.
-
-        try_click_on_load("/html/body/div[1]/div[2]/div[1]/table/tbody/tr/th/div[2]/table/tbody/tr[3]/td[1]/span/a")
-
-        # Now, get the documents page for this case.
-        casePage = driver.current_url
-        documentsPage = casePage + "/documents/new?document_template_id=20426"
-
-        # Open new tab
-
-        driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 't')
-        # Set new tab as focused tab
-        driver.switch_to.window(driver.window_handles[-1])
-
-        # Open documents page
-        driver.execute_script("window.open(\'" + documentsPage + "\')")
-        while True:
-            events = window.Read()
-            print(events)
-            if events[0] == "VSERV":
-                print("made it to hereeee")
-                driver.switch_to.window(driver.window_handles[0])
-                driver.get(casePage + "/victims")
-                print("made it to here?")
-                x = 0
-                while True:
-                    try_click_on_load("/html/body/div[1]/div[2]/div[1]/table/tbody/tr/th/table[2]"
-                                      "/tbody/tr[1]/td/table/tbody/tr/td[2]/a[1]")
-                    elem = driver.find_elements_by_xpath("//*[contains(text(), 'Victim:')]")
-                    print([elements.text for elements in elem])
-                    try_click_on_load("/html/body/div[1]/div[2]/div[1]/table/tbody/tr/th/table[2]"
-                                      "/tbody/tr[1]/td/table/tbody/tr/td[2]/a[1]")
-                    elem = driver.find_elements_by_xpath("//*[contains(text(), 'Victim:')]")
-                    elem[x].click()
-                    try_click_on_load("//*[@id=\"vssr_service_kwid_1212\"]")
-                    try_click_on_load("//*[@id=\"vssr_service_kwid_1211\"]")
-                    try_text_on_load("//*[@id=\"victim_service_service_note\"]", "CC-JT letter sent.")
-                    if x == len(elem) - 1:
-                        break
-                    x += 1
-            elif events[0] == "Submit":
-                break
-            elif events[0] == "Cancel":
-                break
-            else:
-                continue
-        driver.switch_to.window(driver.window_handles[0])
+        letter_setup("CC-JT letter sent.", "/documents/new?document_template_id=20426")
     elif values[4]:
-        window.Element('VSERV').Update(visible=True)
-        # Initialize variables
-        baseYear = values[0]
-        year = baseYear[-2] + baseYear[-1]
-        caseNum = values[1]
-        numJTDates = ""
-        jtDateSecond = ""
-        # Construct case number
-        fullCaseNum = "ST-" + str(year) + "-CR-" + str(caseNum)
-
-        # Try to enter case number
-
-        search_case_from_case_num(fullCaseNum)
-
-        # Click on case.
-
-        try_click_on_load("/html/body/div[1]/div[2]/div[1]/table/tbody/tr/th/div[2]/table/tbody/tr[3]/td[1]/span/a")
-
-        # Now, get the documents page for this case.
-        casePage = driver.current_url
-        documentsPage = casePage + "/documents/new?document_template_id=20787"
-
-        # Open new tab
-
-        driver.find_element_by_tag_name('body').send_keys(Keys.CONTROL + 't')
-        # Set new tab as focused tab
-        driver.switch_to.window(driver.window_handles[-1])
-
-        # Open documents page
-        driver.execute_script("window.open(\'" + documentsPage + "\')")
-        while True:
-            events = window.Read()
-            if events[0] == "VSERV":
-                print("made it to hereeee")
-                driver.switch_to.window(driver.window_handles[0])
-                driver.get(casePage + "/victims")
-                print("made it to here?")
-                x = 0
-                while True:
-                    try_click_on_load("/html/body/div[1]/div[2]/div[1]/table/tbody/tr/th/table[2]"
-                                      "/tbody/tr[1]/td/table/tbody/tr/td[2]/a[1]")
-                    elem = driver.find_elements_by_xpath("//*[contains(text(), 'Victim:')]")
-                    print([elements.text for elements in elem])
-                    try_click_on_load("/html/body/div[1]/div[2]/div[1]/table/tbody/tr/th/table[2]"
-                                      "/tbody/tr[1]/td/table/tbody/tr/td[2]/a[1]")
-                    elem = driver.find_elements_by_xpath("//*[contains(text(), 'Victim:')]")
-                    elem[x].click()
-                    try_click_on_load("//*[@id=\"vssr_service_kwid_1212\"]")
-                    try_click_on_load("//*[@id=\"vssr_service_kwid_1211\"]")
-                    try_text_on_load("//*[@id=\"victim_service_service_note\"]", "Continued CC letter sent.")
-                    if x == len(elem)-1:
-                        break
-                    x += 1
-            elif events[0] == "Submit":
-                break
-            elif events[0] == "Cancel":
-                break
-            else:
-                continue
-        driver.switch_to.window(driver.window_handles[0])
+        letter_setup("CC letter sent.", "/documents/new?document_template_id=20787")
     elif values[5]:
-        print("a")
-        #CALL EVIDENCE REQ LOOP
+        go_to_case(values)
+        if try_find_element("//*[contains(text(), 'Evidence Request Form 9-5-17')]"):
+            window.Element('ERQPRESENT').Update(visible=True)
+            break
+        else:
+            erq_setup("/documents/new?document_template_id=17572")
+
+
     elif values[6]:
-        print("todo, case status update")
-    elif values[7]:
-        print("todo, change of plea")
+        letter_setup("Case Status letter sent.", "/documents/new?document_template_id=19308")
     else:
         print("ERROR NO RADIO BUTTON SELECTED")
         exit()
-    if event == "Cancel":
-        exit()
-    if event == "Submit":
-        continue
-
